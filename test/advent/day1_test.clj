a4478a47cbeaabda9636b836cab1cd1ab2d93ca8
Bob Follek <bfollek@gmail.com>
2018-12-02 19:36:24 -0500

    Finished day 1

--- test/advent/day1_test.clj
+++ test/advent/day1_test.clj
@@ -5,3 +5,7 @@
 (deftest test-day1
   (testing "day1 from file"
     (is (= 592 (day1 "data/day1.txt")))))
+
+(deftest test-day1-part2
+  (testing "day1-part2 from file"
+    (is (= 241 (day1-part2 "data/day1.txt")))))
