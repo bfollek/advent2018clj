814d71fbe9f1609cb1ceb415abd13fb823c8ec8a
Bob Follek <bfollek@gmail.com>
2018-12-09 11:46:31 -0500

    Added a test

--- test/advent/day4_test.clj
+++ test/advent/day4_test.clj
@@ -3,5 +3,7 @@
             [advent.day4 :refer :all]))
 
 (deftest test-day4-part1
-  (testing "strategy-1"
-    (is (= 109716 (strategy-1)))))
+  (testing "load-timestamps"
+    (is (= 1045 (count (load-timestamps))))))
+  ;;(testing "strategy-1"
+    ;;(is (= ? (strategy-1)))))
