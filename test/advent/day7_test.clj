685725d6dbc1e59d6ecd8aa57056de74716048e7
Bob Follek <bfollek@gmail.com>
2018-12-14 22:45:36 -0500

    Started day 7

---
+++ test/advent/day7_test.clj
@@ -0,0 +1,7 @@
+(ns advent.day7-test
+  (:require [clojure.test :refer :all]
+            [advent.day6 :refer :all]))
+
+(deftest test-day7-part1
+  (testing "part1"
+    (is (= 0 (part1 "data/day7.txt")))))
