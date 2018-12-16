58915bb82a0ddd1f603f5abeaa700b0f96fb231e
Bob Follek <bfollek@gmail.com>
2018-12-16 14:59:58 -0500

    WIP

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -40,6 +40,39 @@
 
 ; In what order should the steps in your instructions be completed?
 
+; --- Part Two ---
+
+; As you're about to begin construction, four of the Elves offer to help. "The sun will set soon; it'll go faster if we work together." Now, you need to account for multiple people working on steps simultaneously. If multiple steps are available, workers should still begin them in alphabetical order.
+
+; Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+
+; To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers) and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds). Then, using the same instructions as above, this is how each second would be spent:
+
+; Second   Worker 1   Worker 2   Done
+;    0        C          .
+;    1        C          .
+;    2        C          .
+;    3        A          F       C
+;    4        B          F       CA
+;    5        B          F       CA
+;    6        D          F       CAB
+;    7        D          F       CAB
+;    8        D          F       CAB
+;    9        D          .       CABF
+;   10        E          .       CABFD
+;   11        E          .       CABFD
+;   12        E          .       CABFD
+;   13        E          .       CABFD
+;   14        E          .       CABFD
+;   15        .          .       CABFDE
+; Each row represents one second of time. The Second column identifies how many seconds have passed as of the beginning of that second. Each worker column shows the step that worker is currently doing (or . if they are idle). The Done column shows completed steps.
+
+; Note that the order of the steps has changed; this is because steps now take time to finish and multiple workers can begin multiple steps simultaneously.
+
+; In this example, it would take 15 seconds for two workers to complete these steps.
+
+; With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
+
 (defn found-step
   "Found-step maintains the `steps->waiting-for` map by adding keys and values as necessary.
    Each key is a step name, and each value is a coll of step names that the
@@ -72,17 +105,14 @@
        sort
        first))
 
-(defn update-step
-  [steps->waiting-for step-name finished-step-name]
-  (println "step-name:" step-name "finished-step-name:" finished-step-name)
-  (update steps->waiting-for step-name (remove (partial = finished-step-name) (steps->waiting-for step-name))))
-
 (defn finished-step
   [steps->waiting-for finished-step-name]
   ;; Remove the step we finished...
   (let [steps->waiting-for (dissoc steps->waiting-for finished-step-name)]
     ;; And remove it from the waiting-for coll in any other steps
-    (into {} (for [[k v] steps->waiting-for] [k (remove (partial = finished-step-name) v)]))))
+    (apply merge (for [[k v] steps->waiting-for] {k (remove (partial = finished-step-name) v)}))))
+    ;; Another (for) way
+    ;; (into {} (for [[k v] steps->waiting-for] [k (remove (partial = finished-step-name) v)]))))
     ;; This works, but seems considerably harder to read than the (for) version
     ;; (reduce #(update %1 %2 (fn [waiting-for] (remove (partial = finished-step-name) waiting-for)))
     ;;           steps->waiting-for (keys steps->waiting-for)))
