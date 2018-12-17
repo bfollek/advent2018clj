a29e10376abddb4af9bcb820e45788df82f87ce5
Bob Follek <bfollek@gmail.com>
2018-12-16 20:46:52 -0500

    finished-step accepts multiple finished-names ATP

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -115,8 +115,9 @@
         (take n))))
 
 (defn update-waiting-for
-  [steps step-name finished-name]
-  (update steps step-name (fn [waiting-for] (remove #(= finished-name %) waiting-for))))
+  [steps step-name finished-names]
+  ;; Untested after the change to multiple finished-names...
+  (update steps step-name (fn [waiting-for] (remove (set finished-names) waiting-for))))
 
 (defn finished-step
   [steps & finished-names]
@@ -125,9 +126,9 @@
     ;; And remove it from the waiting-for coll in any other steps
     (apply merge (for [[k v] steps] {k (remove (set finished-names) v)}))))
     ;; Another (for) way
-    ;; (into {} (for [[k v] steps] [k (remove (partial = finished-name) v)]))))
+    ;; (into {} (for [[k v] steps] [k (remove (set finished-names) v)]))))
     ;; This works, but seems considerably harder to read than the (for) version
-    ;; (reduce #(update %1 %2 (fn [waiting-for] (remove (partial = finished-name) waiting-for)))
+    ;; (reduce #(update %1 %2 (fn [waiting-for] (remove (set finished-names) waiting-for)))
     ;;           steps (keys steps)))
     ;; This works, but uses another func to hide some of the mess. (for) still looks like the winner.
     ;; (reduce #(update-waiting-for %1 %2 finished-name) steps (keys steps))))
