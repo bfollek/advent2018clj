aef0960af95574669618d69896b378f4b7d8b169
Bob Follek <bfollek@gmail.com>
2019-05-08 13:59:02 -0400

    WIP

--- test/advent/day01_test.clj
+++ test/advent/day01_test.clj
@@ -2,10 +2,8 @@
   (:require [clojure.test :refer :all]
             [advent.day01 :refer :all]))
 
-(deftest test-day01
-  (testing ""
-    (is (= 592 (sum-freqs "data/day01.txt")))))
+(deftest test-day01-part1
+  (is (= 592 (sum-freqs "data/day01.txt"))))
 
 (deftest test-day01-part2
-  (testing ""
-    (is (= 241 (find-repeated-sum "data/day01.txt")))))
+  (is (= 241 (find-repeated-sum "data/day01.txt"))))
