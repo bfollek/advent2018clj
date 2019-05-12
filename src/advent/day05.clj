97de35f31c0b3d89a4dfb4070b5c3a24acf2dd6d
Bob Follek <bfollek@gmail.com>
2019-05-12 06:58:56 -0400

    Performance tweak ATP

--- src/advent/day05.clj
+++ src/advent/day05.clj
@@ -19,16 +19,18 @@
      (reduce (fn [v c] (cond
                          (skip-char? c) v
                          ;; Skip c. v doesn't change.
-                         (and (seq v) (reaction? (last v) c)) (subvec v 0 (dec (count v)))
+                         (and (seq v) (reaction? (last v) c)) (pop v)
                          ;; Skip c and drop the last char of v.
                          ;; Keep v a vector so that conj works right.
-                         ;; First I tried (butlast) or (drop-last)
-                         ;; wrapped in (vec) or (into []). They were
-                         ;; about the same speed: more than twice as slow as (subvec).
                          ;; The (seq v) check makes sure v isn't empty. This comes up
                          ;; if the polymer starts with a reactive unit pair. It's removed,
                          ;; and (last v) is nil the next time through, and (reaction?)
                          ;; blows up if we call it.
+                         ;;
+                         ;; PERFORMANCE NOTES:
+                         ;; First I tried (butlast) or (drop-last) wrapped in (vec) or (into []).
+                         ;; They were about the same speed: more than twice as slow as (subvec).
+                         ;; (pop) is about 10% faster (than subvec).
                          :else (conj v c)))
                          ;; Keep c
              [(first s)] (rest s)))))
