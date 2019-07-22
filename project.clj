af8c503f7b971cdfa3d027b902fea493c7b78ab4
Bob Follek <bfollek@gmail.com>
2019-07-22 10:13:13 -0400

    Update clojure to 1.10.1

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojars.bfollek/rabbithole "0.2.2-SNAPSHOT"]])
